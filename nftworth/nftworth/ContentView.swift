9c21a0f2069893f31ab696528e88bed6e3f87d62
niantony <66891025+niantony@users.noreply.github.com>
2022-04-03 09:14:16 -0400

    init

---
+++ nftworth/nftworth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nftworth
+//
+//  Created by Antony Ni on 4/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
